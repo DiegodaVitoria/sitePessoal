ea02a037c27708686e66cef794077c13805c4710
Diego Carlos Reis da Vitoria <53801283+DiegodaVitoria@users.noreply.github.com>
2021-05-27 23:56:36 -0300

    updates and corrects

--- script.js
+++ script.js
@@ -1,55 +1,74 @@
-$(document).ready(function(){
- 
- /*=====================menu bar====================*/
- $(window).scroll(function(){
-  if(this.scrollY > 20){
-   $('.navbar').addClass("sticky")
-  }else{
-   $('.navbar').removeClass("sticky")
-  }
- });
-/*=====================hotdog button====================*/
-  $('.menu-btn').click(function(){
-   $('.navbar .menu').toggleClass("active");
-   $('.menu-btn i').toggleClass("active");
- });
+$(document).ready(function () {
 
- // oewl carousel script
-
- $('.carousel').owlCarousel({
-  margin: 20,
-  loop: true,
-  autoplayTimeOut: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0:{
-      items: 1,
-      nav: false,
-    },
-    600:{
-      items: 2,
-      nav: false,
-    },
-    1000:{
-      items: 3,
-      nav: false,
+  /*=====================menu bar====================*/
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
     }
+    if ($(this).scrollTop() > 500) {
+      $('.scroll-up-btn').addClass('header-scrolled');
+    } else {
+      $('.scroll-up-btn').removeClass('header-scrolled');
+    }
+  });
+  /*=====================scroll button====================*/
+
+  if ($(window).scrollTop() > 500) {
+    $('.scroll-up-btn').addClass('header-scrolled');
   }
- });
+  /*=====================animation script====================*/
+  var typed = new Typed(".typing", {
+    strings: ["Desenvolvedor", "Analista", "Programador"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
 
- /*=====================Smooth Scroll====================*/
- $("a").on('click', function(event) {
+  /*=====================hotdog button====================*/
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
 
-  if (this.hash !== "") {
-    event.preventDefault();
-    var hash = this.hash;
+  // oewl carousel script
 
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
-      window.location.hash = hash;
-    });
-  }
-});
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    dots: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      }
+    }
+  });
 
+  /*=====================Smooth Scroll====================*/
+  $("a").on('click', function (event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+  
 });
